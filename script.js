69408b08d0d094c51c96aee2a2215fe1bcd15749
Vander-Graaf <138441872+Vander-Graaf@users.noreply.github.com>
2023-12-29 23:04:02 +0600

    v1.04

--- script.js
+++ script.js
@@ -5,23 +5,25 @@ jumpSound.volume = 0.1
 
 canvas.width = 1024;
 canvas.height = 576;
+CanvasPositionX = 0
 
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 
-const gravity = 0.2
+const gravity = 0.4
 
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
-        this.height = 150
+        this.height = 100
+        this.width = 50
     }
 
     draw() {
-        c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = "green"
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
     update() {
@@ -30,31 +32,35 @@ class Sprite {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
+
+       
+
+
+
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
+        } else if (this.position.y <= 0) {
+            keys.w.pressed = false
+            this.velocity.y = 1
         } else {
             this.velocity.y += gravity
-         
         }
+
+
+        if (this.position.x + this.width > canvas.width) {
+            keys.d.pressed = false
+        } else if (this.position.x < 0) {
+            keys.a.pressed = false
+        }
+   
+
+
     }
 
 
 }
 
 const player = new Sprite({
-    position: {
-    x: 0,
-    y: 0
-    },
-    velocity: {
-    x: 0,
-    y: 0
-    }
-})
-
-
-
-const enemy = new Sprite({
     position: {
     x: 400,
     y: 0
@@ -64,6 +70,10 @@ const enemy = new Sprite({
     y: 0
     }
 })
+
+
+
+
 
 const keys = {
     a: {
@@ -84,19 +94,25 @@ function animate() {
     window.requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
-    enemy.update()
-
-
+  
     player.velocity.x = 0
 
 
 
+ 
 
     if (keys.a.pressed && lastkey === 'a') {
-        player.velocity.x = -3
+        player.velocity.x = -7
     } else if (keys.d.pressed  && lastkey === 'd') {
-        player.velocity.x = 3
+        player.velocity.x = 7
     }
+
+    if (player.position.y + player.height >= 576 && keys.w.pressed) {
+        player.velocity.y = - 10
+        jumpSound.pause()
+        jumpSound.currentTime = 0;
+        jumpSound.play()
+        }
    
 }
 
@@ -115,10 +131,7 @@ window.addEventListener('keydown', (event) => {
     lastkey = 'a'
  break
     case 'w':
-        jumpSound.pause()
-        jumpSound.currentTime = 0;
-        jumpSound.play()
-        player.velocity.y = - 10
+    keys.w.pressed = true 
  break
 }
 })
