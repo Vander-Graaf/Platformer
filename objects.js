3c6fdd866f453ba9c39d1aa55fd122d5298c43c4
Vander-Graaf <138441872+Vander-Graaf@users.noreply.github.com>
2024-01-04 15:12:42 +0600

    v1.10

--- objects.js
+++ objects.js
@@ -2,6 +2,363 @@
 
 
 
+class Sprite {
+    constructor({position, velocity, height , width, color, imageSrc, scale = 0.075}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = height
+        this.width = width
+        this.color = color 
+        this.image = new Image()
+        this.image.src = imageSrc
+        this.scale = scale
+    }
+
+    draw() {
+
+   // c.fillStyle = this.color
+   // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+
+      
+        c.drawImage(
+        this.image,
+        39,
+        0,
+        this.image.width / 1,
+        this.image.height / 1,
+        this.position.x,
+        this.position.y,
+        (this.image.width / 1) * this.scale,
+        (this.image.height / 1)* this.scale
+        
+        )
+
+        this.image.src = ''
+
+        if (player.velocity.y !== 0) {
+         onGround = false
+        }
+         
+ 
+        
+         if (this.velocity.x == 0 && this.velocity.y == 0 && onGround && lastkey == 'd') {
+         this.image.src = './img/idle-right.png'
+         } else if (this.velocity.x == 0 && this.velocity.y == 0 && onGround && lastkey == 'a') {
+         this.image.src = './img/idle-left.png'
+         }
+ 
+         if (onGround == false && lastkey == 'd' || player.velocity.y !== 0 && lastkey == 'd'){
+             this.image.src = './img/jump-right.png'
+         } else if (onGround == false && lastkey == 'a'|| player.velocity.y !== 0 && lastkey == 'a') {
+             this.image.src = './img/jump-left.png'
+         }
+ 
+         if (keys.d.pressed && lastkey == 'd' && onGround) {
+           this.image.src = './img/walk-right1.png'
+             
+         } else if (keys.a.pressed && lastkey == 'a' && onGround) {
+           this.image.src = './img/walk-left1.png'
+         } 
+        
+    }
+
+    update() {
+        this.draw()
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+
+        //Колизия с краем экрана
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+       
+        }  else {
+            this.velocity.y += gravity
+        }
+
+
+        if (player.position.x + player.width > canvas.width) {
+            keys.d.pressed = false
+            player.velocity.x = 0
+        } else if (player.position.x < 0) {
+            keys.a.pressed = false
+            player.velocity.x = 0
+        } 
+        //Колизия с краем экрана
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// блоки 
+class Block {
+    constructor({position, velocity, height , width, color, imageSrc, scale = 0.079}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = height
+        this.width = width
+        this.color = color 
+        this.image = new Image()
+        this.image.src = imageSrc
+        this.scale = scale
+    }
+
+    draw() {
+        // c.fillStyle = this.color
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        
+        c.drawImage(
+        this.image,
+        0,
+        0,
+        this.image.width / 1,
+        this.image.height / 1,
+        this.position.x,
+        this.position.y,
+        (this.image.width / 1) * this.scale,
+        (this.image.height / 1)* this.scale,
+        this.image.src = './img/brick.png'
+        )
+
+
+
+       
+    }
+
+
+    
+
+update() {
+        this.draw()
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+
+    
+
+
+
+ if (  // левая коллизия
+    player.position.y + player.height > this.position.y  
+    && this.position.y + this.height  > player.position.y + 15
+    && player.position.x  <= this.position.x 
+    && player.position.x + player.width >= this.position.x 
+    ){
+     player.position.x = this.position.x - 1 - player.width
+    }  
+ 
+ else if (   // правая коллизия
+         player.position.y + player.height > this.position.y 
+         && this.position.y + this.height > player.position.y + 15
+         && player.position.x < this.position.x + this.width 
+         && player.position.x + player.width   > this.position.x 
+         ){
+         player.position.x = this.position.x + 1 + this.width
+         }   
+        
+else if ( //Верхняя коллизия
+        player.position.y + player.height + player.velocity.y > this.position.y
+        && this.position.y + this.height -1 > player.position.y + 15
+        && player.position.x + player.width   > this.position.x 
+        && player.position.x < this.position.x + this.width
+        ){
+       
+        onGround = true
+        player.position.y = this.position.y - player.height
+        player.velocity.y = 0
+        }
+
+        
+else if ( //Нижняя колизия
+        player.position.y + player.height + player.velocity.y > this.position.y
+        && this.position.y + this.height + 5 > player.position.y
+        && player.position.x + player.width   > this.position.x 
+        && player.position.x < this.position.x + this.width
+        && player.velocity.y < 0
+        ){
+         player.position.y = this.position.y + 6 + this.height
+         player.velocity.y = 2
+         bumpSound.pause()
+         bumpSound.currentTime = 0;
+         bumpSound.play()
+        }
+    }
+}
+
+// блоки 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Монеты 
+class Coin {
+    constructor({position, velocity, height , width, color, imageSrc, scale = 0.079}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = height
+        this.width = width
+        this.color = color 
+        this.image = new Image()
+        this.image.src = imageSrc
+        this.scale = scale
+    }
+
+    draw() {
+       // c.fillStyle = this.color
+       // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        
+        c.drawImage(
+        this.image,
+        -45,
+        0,
+        this.image.width / 1,
+        this.image.height / 1,
+        this.position.x,
+        this.position.y,
+        (this.image.width / 1) * this.scale,
+        (this.image.height / 1)* this.scale,
+        this.image.src = './img/coin.gif'
+        )
+
+
+
+       
+    }
+
+
+    
+
+update() {
+        this.draw()
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+
+    
+
+
+
+
+
+      if (  // левая коллизия
+         player.position.y + player.height > this.position.y  
+         && this.position.y + this.height  > player.position.y + 15
+         && player.position.x  <= this.position.x 
+         && player.position.x + player.width >= this.position.x 
+         ){  
+            this.position.x = -200
+            coins += 1
+            coinsPick.innerHTML = `COINS <div class="align-under-stats">${coins}</div>`
+            coinSound.pause()
+            coinSound.currentTime = 0;
+            coinSound.play()
+         }  
+ 
+ else if (   // правая коллизия
+         player.position.y + player.height > this.position.y 
+         && this.position.y + this.height > player.position.y + 15
+         && player.position.x < this.position.x + this.width 
+         && player.position.x + player.width   > this.position.x 
+         ){  
+            this.position.x = -200
+            coins += 1
+            coinsPick.innerHTML = `COINS <div class="align-under-stats">${coins}</div>`
+            coinSound.pause()
+            coinSound.currentTime = 0;
+            coinSound.play()
+         } 
+ else if ( //Верхняя коллизия
+         player.position.y + player.height + player.velocity.y > this.position.y
+         && this.position.y + this.height -1 > player.position.y + 15
+         && player.position.x + player.width   > this.position.x 
+         && player.position.x < this.position.x + this.width
+         ){  
+            this.position.x = -200
+            coins += 1
+            coinsPick.innerHTML = `COINS <div class="align-under-stats">${coins}</div>`
+            coinSound.pause()
+            coinSound.currentTime = 0;
+            coinSound.play()
+         } 
+         
+ else if ( //Нижняя колизия
+         player.position.y + player.height + player.velocity.y > this.position.y
+         && this.position.y + this.height + 5 > player.position.y
+         && player.position.x + player.width   > this.position.x 
+         && player.position.x < this.position.x + this.width
+         ){  
+            this.position.x = -200
+            coins += 1
+            coinsPick.innerHTML = `COINS <div class="align-under-stats">${coins}</div>`
+            coinSound.pause()
+            coinSound.currentTime = 0;
+            coinSound.play()
+         }  
+    }
+}
+// Монеты 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -14,7 +371,7 @@ const player = new Sprite({
     x: 0,
     y: 0
     },
-    width: 40, height: 50, color: 'gold',
+    width: 30, height: 38, color: 'gold',
     imageSrc: ''
 })
 
@@ -24,14 +381,14 @@ const player = new Sprite({
 
 const Box1 = new Block({
     position: {
-        x: 800,
-        y: 370
+        x: 100,
+        y: 350
         },
         velocity: {
         x: 0,
         y: 0
         },  
-        width: 90, height: 280, color: 'black'
+        width: 40, height: 40, color: 'black'
 })
 
 
@@ -39,13 +396,13 @@ const Box1 = new Block({
 const Box2 = new Block({
     position: {
         x: -30,
-        y: 550
+        y: 490
         },
         velocity: {
         x: 0,
         y: 0
         },  
-        width:370, height: 50, color: 'black'
+        width:9070, height: 250, color: 'black'
 })
 
 
@@ -58,33 +415,33 @@ const Box3 = new Block({
         x: 0,
         y: 0
         },  
-        width:690, height: 50, color: 'black'
+        width:290, height: 50, color: 'black'
 })
 
 
 const Box4 = new Block({
     position: {
-        x: 90,
+        x: 600,
         y: 350
         },
         velocity: {
         x: 0,
         y: 0
         },  
-        width:190, height: 50, color: 'black'
+        width:40, height: 40, color: 'black'
 })
 
 
 const Box5 = new Block({
     position: {
-        x: 300,
-        y: 180
+        x: 800,
+        y: 350
         },
         velocity: {
         x: 0,
         y: 0
         },  
-        width:380, height: 50, color: 'black'
+        width:40, height: 40, color: 'black'
 })
 
 
@@ -92,12 +449,39 @@ const Box5 = new Block({
 
 const Box6 = new Block({
     position: {
-        x: 950,
+        x: 760,
         y: 220
         },
         velocity: {
         x: 0,
         y: 0
         },  
-        width:380, height: 50, color: 'black'
+        width:40, height: 40, color: 'black'
+})
+
+
+
+const coin1 = new Coin({
+    position: {
+        x: 600,
+        y: 220
+        },
+        velocity: {
+        x: 0,
+        y: 0
+        },  
+        width:40, height: 40, color: 'black'
+})
+
+
+const coin2 = new Coin({
+    position: {
+        x: 100,
+        y: 220
+        },
+        velocity: {
+        x: 0,
+        y: 0
+        },  
+        width:40, height: 40, color: 'black'
 })
--- script.js
+++ script.js
@@ -3,12 +3,35 @@
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
+
 const jumpSound = new Audio('sounds/jump.mp3')
+const bumpSound = new Audio('sounds/bump.wav')
+const coinSound = new Audio('sounds/coin.wav')
+jumpSound.volume = 0.006
+bumpSound.volume = 0.09
+coinSound.volume = 0.09
+
 const xCoord = document.querySelector('.x-coordinate')
 const yCoord = document.querySelector('.y-coordinate')
 const xVelocity = document.querySelector('.x-velocity')
 const yVelocity = document.querySelector('.y-velocity')
-jumpSound.volume = 0.01
+
+const scorePick = document.querySelector('.score')
+const coinsPick = document.querySelector('.coins')
+const worldPick = document.querySelector('.world')
+const timePick = document.querySelector('.time')
+const livesPick = document.querySelector('.lives')
+let lives = 3
+let score = 0
+let time = 400
+let coins = 0
+
+setInterval(() => {
+    time -= 1
+    timePick.innerHTML = `TIME <div class="align-under-stats">${time}</div>`
+}, 1000);
+
+
 
 canvas.width = 1024
 canvas.height = 576
@@ -20,187 +43,16 @@ onGround = false
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 
-const gravity = 1
+const gravity = 0.4
 let acceleration = 1
 let index = 0
-let lives = 3
+
+
 let touchingWall = false
 let lastkey = 'd'
 
 
-class Sprite {
-    constructor({position, velocity, height , width, color, imageSrc, scale = 0.1}) {
-        this.position = position
-        this.velocity = velocity
-        this.height = height
-        this.width = width
-        this.color = color 
-        this.image = new Image()
-        this.image.src = imageSrc
-        this.scale = scale
-    }
 
-    draw() {
-
-    //  c.fillStyle = this.color
-    //  c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-
-      
-        c.drawImage(
-        this.image,
-        29,
-        0,
-        this.image.width / 1,
-        this.image.height / 1,
-        this.position.x,
-        this.position.y,
-        (this.image.width / 1) * this.scale,
-        (this.image.height / 1)* this.scale
-        
-        )
-
-        this.image.src = ''
-
-        if (player.velocity.y !== 0) {
-         onGround = false
-        }
-         
- 
-        
-         if (this.velocity.x == 0 && this.velocity.y == 0 && onGround && lastkey == 'd') {
-         this.image.src = './img/idle-right.png'
-         } else if (this.velocity.x == 0 && this.velocity.y == 0 && onGround && lastkey == 'a') {
-         this.image.src = './img/idle-left.png'
-         }
- 
-         if (onGround == false && lastkey == 'd' || player.velocity.y !== 0 && lastkey == 'd'){
-             this.image.src = './img/jump-right.png'
-         } else if (onGround == false && lastkey == 'a'|| player.velocity.y !== 0 && lastkey == 'a') {
-             this.image.src = './img/jump-left.png'
-         }
- 
-         if (keys.d.pressed && lastkey == 'd' && onGround) {
-             this.image.src = './img/walk-right1.png'
-         } else if (keys.a.pressed && lastkey == 'a' && onGround) {
-           this.image.src = './img/walk-left1.png'
-         } 
-        
-    }
-
-    update() {
-        this.draw()
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-
-        //Колизия с краем экрана
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-       
-        } else if (this.position.y <= 0) {
-            keys.w.pressed = false
-            this.velocity.y = 5
-        } else {
-            this.velocity.y += gravity
-        }
-
-
-        if (player.position.x + player.width > canvas.width) {
-            keys.d.pressed = false
-            player.velocity.x = 0
-        } else if (player.position.x < 0) {
-            keys.a.pressed = false
-            player.velocity.x = 0
-        } 
-        //Колизия с краем экрана
-
-
-
-    }
-}
-
-
-
-
-// блоки 
-class Block {
-    constructor({position, velocity, height , width, color}) {
-        this.position = position
-        this.velocity = velocity
-        this.height = height
-        this.width = width
-        this.color = color 
-    }
-
-    draw() {
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-
-update() {
-        this.draw()
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-
-
-
-
-
- if (  // левая коллизия
-    player.position.y + player.height > this.position.y  
-    && this.position.y + this.height  > player.position.y + 15
-    && player.position.x  <= this.position.x 
-    && player.position.x + player.width >= this.position.x 
-    ){
-     player.position.x = this.position.x - 1 - player.width
-    }  
- 
- else if (   // правая коллизия
-         player.position.y + player.height > this.position.y 
-         && this.position.y + this.height > player.position.y + 15
-         && player.position.x < this.position.x + this.width 
-         && player.position.x + player.width   > this.position.x 
-         ){
-         player.position.x = this.position.x + 1 + this.width
-         }   
-        
-else if ( //Верхняя коллизия
-        player.position.y + player.height + player.velocity.y > this.position.y
-        && this.position.y + this.height -1 > player.position.y + 15
-        && player.position.x + player.width   > this.position.x 
-        && player.position.x < this.position.x + this.width
-        ){
-       
-        onGround = true
-        player.position.y = this.position.y - player.height
-        player.velocity.y = 0
-        }
-
-        
-else if ( //Нижняя колизия
-        player.position.y + player.height + player.velocity.y > this.position.y
-        && this.position.y + this.height + 5 > player.position.y
-        && player.position.x + player.width   > this.position.x 
-        && player.position.x < this.position.x + this.width
-        ){
-         player.position.y = this.position.y + 6 + this.height
-         player.velocity.y = 2
-        }
-     
-
-    
-
-        
-        
- 
-  
-    }
-}
-
-// блоки 
 
 
 
@@ -247,9 +99,11 @@ function animate() {
     Box4.update()
     Box5.update()
     Box6.update()
+    coin1.update()
+    coin2.update()
 
     player.update()
-
+   
   
 
     player.velocity.x = 0
@@ -258,16 +112,16 @@ function animate() {
 //Управление
     if (keys.w.pressed && onGround) {
         onGround = false
-        player.velocity.y = - 20
+        player.velocity.y = - 11
         jumpSound.pause()
         jumpSound.currentTime = 0;
         jumpSound.play()
         }
   
     if (keys.a.pressed && lastkey === 'a' ) {
-        player.velocity.x = -5
+        player.velocity.x = -4
      } else if (keys.d.pressed  && lastkey === 'd' ) {
-         player.velocity.x = 5
+         player.velocity.x = 4
      }
 //Управление
 
